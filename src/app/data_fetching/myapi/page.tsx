0fb755151f48df26f320b14eef3493cb83e836f0
omChotaliya2002 <omchotaliya2005@gmail.com>
2025-05-28 10:39:38 +0530

    Made some changes

--- src/app/data_fetching/myapi/page.tsx
+++ src/app/data_fetching/myapi/page.tsx
@@ -4,8 +4,8 @@ import React from 'react';
 
 const fetchProtectedData = async () => {
 
-    const res = await fetch("http://localhost:3000/myapi/protected", {
-        method : "GET",
+    const res = await fetch("/myapi/protected", {
+        method : "GET`",
         headers : {
             Authorization : "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c",
         },
